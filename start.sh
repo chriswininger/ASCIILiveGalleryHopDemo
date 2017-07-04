b39273b9d90f8f5ac748798d3739828c83e95bb9
chriswininger <chris.wininger@gmail.com>
2017-07-04 10:37:06 -0400

    Created infrstructure to launch the application from an icon   under the ubuntu mate machine being used for the gallery

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source ~/.bashrc
+source ~/.nvm/nvm.sh
+cd /home/chris/ASCIILiveGalleryHopDemo
+source ~/.bashrc
+source ~/.nvm/nvm.sh
+export TERM=xterm-256color
+~/.nvm/versions/node/v6.9.2/bin/node /home/chris/ASCIILiveGalleryHopDemo/index.js
+#read -p "Press enter to continue"
+

37b85781e921be2b473ae7add1198ac7d98602e1
chriswininger <chris.wininger@gmail.com>
2017-07-08 16:36:22 -0400

    upgrade mongo

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ cd /home/chris/ASCIILiveGalleryHopDemo
 source ~/.bashrc
 source ~/.nvm/nvm.sh
 export TERM=xterm-256color
+node ./lib/fileWatcher.js &&
 ~/.nvm/versions/node/v6.9.2/bin/node /home/chris/ASCIILiveGalleryHopDemo/index.js
 #read -p "Press enter to continue"
 

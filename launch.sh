f7f0b068435c3c8717062fc86959e275c0fa7987
chriswininger <chris.wininger@gmail.com>
2017-07-08 19:15:57 -0400

    added launcher

---
+++ launch.sh
@@ -0,0 +1,2 @@
+/home/chris/ASCIILiveGalleryHopDemo/startFileWatcher.sh &
+mate-terminal --load-config=/home/chris/ASCIILiveGalleryHopDemo//mateTerminal.conf  -e '/home/chris/ASCIILiveGalleryHopDemo/start.sh'

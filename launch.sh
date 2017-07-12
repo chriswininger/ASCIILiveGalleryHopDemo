06f112c1e870be618c42d6c6e090bd4a6fa55b89
chriswininger <chris.wininger@gmail.com>
2017-07-12 08:28:06 -0400

    Don't add get pull yet, pw will cause issues

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
-git pull &&
+#git pull &&
 /home/chris/ASCIILiveGalleryHopDemo/startFileWatcher.sh &
 mate-terminal --load-config=/home/chris/ASCIILiveGalleryHopDemo//mateTerminal.conf  -e '/home/chris/ASCIILiveGalleryHopDemo/start.sh'
